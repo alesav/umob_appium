d3bed346e0d21e99d2a37d5f2f6101aed9bfef11
alekswhim <aleks.whim@gmail.com>
2025-01-15 15:20:52 +0300

    spec changed to spec1

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -5,7 +5,7 @@ export const config: WebdriverIO.Config = {
     ...baseConfig,
 
     //specs: ["/Users/alesav/Dev/umob/appium-boilerplate/tests/specs/book/bookLime.spec.ts"],
-    specs: ["c:/dev/umob_appium/tests/specs/book/bookTaxi.spec.ts"],
+    specs: ["c:/dev/umob_appium/tests/specs/book/bookTaxi.spec1.ts"],
     capabilities: [
         {
             platformName: "Android",
