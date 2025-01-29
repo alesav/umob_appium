b776a0d920c64c73fb9fda011c8d4c146ab561fc
alekswhim <aleks.whim@gmail.com>
2025-01-29 14:17:42 +0300

    added folders with tests for new user and for new user with unlimited voucher

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -4,13 +4,8 @@ import { config as baseConfig } from "./wdio.shared.local.appium.conf.js";
 export const config: WebdriverIO.Config = {
     ...baseConfig,
 
-<<<<<<< HEAD
-    //specs: ["/Users/alesav/Dev/umob/appium-boilerplate/tests/specs/book/bookLime.spec.ts"],
-    specs: ["c:/dev/umob_appium/tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts"],
-=======
     specs: ["/Users/alesav/Dev/umob/appium-boilerplate/tests/specs/book/*.spec.ts"],
     //specs: ["c:/dev/umob_appium/tests/specs/book/reserveFelyx.spec.ts"],
->>>>>>> 798e136c6282ad2ac70fe903c822f37b68ca8c39
     capabilities: [
         {
             platformName: "Android",
