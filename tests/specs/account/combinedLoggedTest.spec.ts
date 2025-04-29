a9c12a03be879c307fb19b66bea9f7a793062664
alekswhim <aleks.whim@gmail.com>
2025-04-29 18:39:04 +0300

    scroll improvement

--- tests/specs/account/combinedLoggedTest.spec.ts
+++ tests/specs/account/combinedLoggedTest.spec.ts
@@ -215,7 +215,7 @@ const { width, height } = await driver.getWindowSize();
 await driver.pause(2000);
 await driver.executeScript('mobile: scrollGesture', [{
   left: width/2,
-  top: height * 0.3, // 0.5 to begin with the middle of the screen or 0.3 to begin from the upper side of the screen 
+  top: height * 0.2, // 0.5 to begin with the middle of the screen or 0.3 to begin from the upper side of the screen 
   width: width * 0.8,
   height: height * 0.3,
   direction: 'down',
