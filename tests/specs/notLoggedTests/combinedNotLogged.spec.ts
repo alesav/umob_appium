06c64fb570d7b8751b2c31d426ce04926ce7b5c0
alekswhim <aleks.whim@gmail.com>
2025-05-02 16:07:31 +0300

    not logged test improvement

--- tests/specs/notLoggedTests/combinedNotLogged.spec.ts
+++ tests/specs/notLoggedTests/combinedNotLogged.spec.ts
@@ -241,7 +241,7 @@ for (let i = 0; i < 2; i++) {
   await driver.pause(2000);
   await driver.executeScript('mobile: scrollGesture', [{
   left: width/2,
-  top: height * 0.2,
+  top: height * 0.1,
   width: width * 0.8,
   height: height * 0.7,
   direction: 'down',
