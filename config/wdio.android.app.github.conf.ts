538fba042de9965e6258e4ef9046c656bf3958c5
alekswhim <aleks.whim@gmail.com>
2025-05-04 21:53:25 +0300

    added all tests to github actions for every night update status

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -5,8 +5,11 @@ export const config: WebdriverIO.Config = {
     ...baseConfig,
 
     specs: [
-        //"../tests/specs/account/*.spec.ts",
-       // "../tests/specs/login/*.spec.ts",
+        "../tests/specs/account/*.spec.ts",
+        "../tests/specs/login/*.spec.ts",
+        "../tests/specs/book/*.spec.ts",
+        "../tests/specs/newUserVoucher/*.spec.ts",
+        "../tests/specs/newUser/*.spec.ts",
         "../tests/specs/notLoggedTests/*.spec.ts",
        // "../tests/specs/book/failedPaymentNoBooking.spec.ts",
        // "../tests/specs/book/reserveCheck.spec.ts",
