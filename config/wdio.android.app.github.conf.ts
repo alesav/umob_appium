e858e7e71248d3b7a2322cf82ca1ea59d0660829
alekswhim <aleks.whim@gmail.com>
2025-05-14 19:31:19 +0300

    Updating popupAddPaymentMethod, nReserveFelyxNoId, , vBookCheck, vBookDonkeyMocked

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -38,13 +38,14 @@ export const config: WebdriverIO.Config = {
     //      "../tests/specs/account/combinedLoggedTest.spec.ts",
           //  "../tests/specs/book/failedPaymentNoBooking.spec.ts",
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
+        "../tests/specs/newUser/nReserveNoId.spec.ts",
         "../tests/specs/newUser/popupAddPaymentMethod.spec.ts",
         "../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts",
-        "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
+        "../tests/specs/newUserVoucher/vBookCheck.spec.ts"
     //       "../tests/specs/book/bookUmobScooters.spec.ts",
         //  "../tests/specs/account/combinedLoggedTest.spec.ts",
         //  "../tests/specs/login/login.negative.spec.ts",
-          "../tests/specs/newUser/nReserveFelyxNoId.spec.ts"
+          
         
 
         // "../tests/specs/book/bookDonkeyMocked.spec.ts",
