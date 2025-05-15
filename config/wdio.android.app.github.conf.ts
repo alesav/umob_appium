96309a59a61e2d19fef1ffce48b2769dcc429c2c
alekswhim <aleks.whim@gmail.com>
2025-05-15 15:46:19 +0300

    update vCombinedLoggedTest

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -30,7 +30,9 @@ export const config: WebdriverIO.Config = {
        // "../tests/specs/account/combinedLoggedTest.spec.ts",         
         // "../tests/specs/book/reserveCheck.spec.ts",
         //  "../tests/specs/book/reserveFelyx.spec.ts",
-          "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts"
+          "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
+          "../tests/specs/account/*.spec.ts"
+
         //  "../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         //    "../tests/specs/newUser/nReserveDonkeyNoCard.spec.ts",
         //    "../tests/specs/newUser/popupAddPaymentMethod.spec.ts",
