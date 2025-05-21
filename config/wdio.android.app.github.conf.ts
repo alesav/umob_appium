8fa7005f9cdfbe58195808ccb8a782722ddfb3d0
alekswhim <aleks.whim@gmail.com>
2025-05-21 15:17:36 +0300

    run some tests with changes

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -10,7 +10,7 @@ export const config: WebdriverIO.Config = {
             "../tests/specs/book/bookTaxi.spec.ts",
             "../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
             "../tests/specs/book/failedPaymentNoBooking.spec.ts",
-            "../tests/specs/account/combinedLoggedTest.spec.ts",
+            //"../tests/specs/account/combinedLoggedTest.spec.ts",
             "../tests/specs/book/bookUmobScooters.spec.ts",
             "../tests/specs/book/bookTaxi.spec.ts"
           
