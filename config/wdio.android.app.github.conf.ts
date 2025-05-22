a2a3af76a9e8c7a48f51ca51703825039be50688
alekswhim <aleks.whim@gmail.com>
2025-05-22 21:35:52 +0300

    night run with updates in tests

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -15,11 +15,11 @@ export const config: WebdriverIO.Config = {
             "../tests/specs/book/bookTaxi.spec.ts"
             */
            
-           "../tests/specs/account/combinedLoggedTest.spec.ts",
-           "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-           "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts"
+        //    "../tests/specs/account/combinedLoggedTest.spec.ts",
+        //    "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
+        //    "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts"
           
-/*
+
             "../tests/specs/account/*.spec.ts",
           "../tests/specs/login/*.spec.ts",
           "../tests/specs/book/*.spec.ts",
@@ -34,7 +34,7 @@ export const config: WebdriverIO.Config = {
           //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
           "../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
           "../tests/specs/newUser/addVoucher.spec.ts"
-          */
+          
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
