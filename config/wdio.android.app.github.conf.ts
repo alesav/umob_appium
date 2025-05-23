2e4f9a8d3e7c9704038724ec16194911c96bbce9
alekswhim <aleks.whim@gmail.com>
2025-05-23 18:12:45 +0300

    test run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -18,6 +18,9 @@ export const config: WebdriverIO.Config = {
            
             // "../tests/specs/account/combinedLoggedTest.spec.ts",
             // "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts"
+             "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
+             "../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts",
+             "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
             "../tests/specs/newUser/nCombinedLoggedTest.spec.ts"
           
 /*
