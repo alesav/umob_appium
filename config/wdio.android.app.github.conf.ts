30003bc2a3ec2f71a5daccb87a8ac8a91eefef7c
alekswhim <aleks.whim@gmail.com>
2025-05-31 01:15:48 +0300

    night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -7,8 +7,8 @@ export const config: WebdriverIO.Config = {
     specs: [
         
 
-             "../tests/specs/book/bookUmobScooters.spec.js",
-            "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js",
+           //  "../tests/specs/book/bookUmobScooters.spec.js",
+           // "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js",
             //  "../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
             //  "../tests/specs/newUser/nReserveDonkeyNoCard.spec.ts"
             // "../tests/specs/book/failedPaymentNoBooking.spec.ts",
@@ -23,9 +23,9 @@ export const config: WebdriverIO.Config = {
             //  "../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts",
             //  "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
              //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts"
-             "../tests/specs/notLoggedTests/*.spec.ts"
+             //"../tests/specs/notLoggedTests/*.spec.ts"
           
-/*
+
             "../tests/specs/account/*.spec.ts",
           "../tests/specs/login/*.spec.ts",
           "../tests/specs/notLoggedTests/*.spec.ts",
@@ -41,7 +41,7 @@ export const config: WebdriverIO.Config = {
           //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
           "../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
           "../tests/specs/newUser/addVoucher.spec.ts"// voucher could be added to different users
-          */
+          
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
