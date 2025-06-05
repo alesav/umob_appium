be92d235e08446bee7c8df7ffb0965948c8f6b89
alekswhim <aleks.whim@gmail.com>
2025-06-05 11:47:08 +0300

    day run

--- tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts
+++ tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts
@@ -2194,7 +2194,7 @@ await driver.performActions([
           id: 'finger4',
           parameters: { pointerType: 'touch' },
           actions: [
-              { type: 'pointerMove', duration: 0, x: width/2, y: 500 },
+              { type: 'pointerMove', duration: 0, x: width/2, y: 900 },
               { type: 'pointerDown', button: 0 },
               { type: 'pause', duration: 100 },
               { type: 'pointerMove', duration: 1000, x: width/2, y: 100 },
