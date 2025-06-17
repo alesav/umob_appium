a0027bba552b4b10325367b2b6308460b315d93d
alekswhim <aleks.whim@gmail.com>
2025-06-17 17:33:27 +0300

    public transport

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -38,7 +38,7 @@ export const config: WebdriverIO.Config = {
             // "../tests/specs/account/combinedLoggedTest.spec.ts",
             // "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
             //  "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
-              "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
+             /// "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
               "../tests/specs/book/bookPublicTransport.spec.ts"
 
             //  "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
