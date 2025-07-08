3dc1253fe7a35d90ea075aba27e8b2c47c23be1c
alekswhim <aleks.whim@gmail.com>
2025-07-08 11:15:03 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -85,11 +85,11 @@ export const config: WebdriverIO.Config = {
          //"../tests/specs/book/bookPublicTransport.spec.ts", //more
          //"../tests/specs/book/bookUmobScooters.spec.js", //more
         // "../tests/specs/book/reserveFelyx.spec.ts", //more
-         "../tests/specs/book/bookTaxi.spec.ts",
-         "../tests/specs/book/failedPaymentNoBooking.spec.ts" //more
+        // "../tests/specs/book/bookTaxi.spec.ts",
+         //"../tests/specs/book/failedPaymentNoBooking.spec.ts" //more
          //"../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
          //"../tests/specs/book/bookDonkeyMocked.spec.ts",
-         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", ok
+         "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js" //ok
          //"../tests/specs/newUser/popupAddPaymentMethod.spec.ts",
          //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
          //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts"
