2c23162979eb32102f6738d260b940aa75dcfa87
alekswhim <aleks.whim@gmail.com>
2025-07-08 12:28:09 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -86,7 +86,7 @@ export const config: WebdriverIO.Config = {
          //"../tests/specs/book/bookUmobScooters.spec.js", //more
         // "../tests/specs/book/reserveFelyx.spec.ts", //more
         // "../tests/specs/book/bookTaxi.spec.ts",
-         //"../tests/specs/book/failedPaymentNoBooking.spec.ts" //more
+         "../tests/specs/book/failedPaymentNoBooking.spec.ts", //more
          //"../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
          //"../tests/specs/book/bookDonkeyMocked.spec.ts",
          "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js" //ok
