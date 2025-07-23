a0dbbf7e623bd4a7b3ab38392812a04e1f7ac35d
alekswhim <aleks.whim@gmail.com>
2025-07-23 12:23:51 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -84,7 +84,7 @@ export const config: WebdriverIO.Config = {
         // "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        //"../tests/specs/book/bookTaxi.spec.ts",
+        "../tests/specs/book/bookTaxi.spec.ts",
         // "../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
@@ -103,7 +103,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts", // fail because payment card wasnt added
         //"../tests/specs/newUser/deleteCard.spec.ts",
          ////////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-         "../tests/specs/book/reserveCheck.spec.ts",
+        // "../tests/specs/book/reserveCheck.spec.ts",
         // "../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts" //taking photo crashed app. Cant stop active booking for new32 user!
         //"../tests/specs/login/login.negative.spec.ts"
--- tests/specs/book/bookTaxi.spec.ts
+++ tests/specs/book/bookTaxi.spec.ts
@@ -322,6 +322,35 @@ await browser.action('pointer', { parameters: { pointerType: 'touch' }})
             ]);
             await driver.pause(1000);
 
+            //extra scroll because of amount of operators
+            await driver.performActions([
+                {
+                    type: "pointer",
+                    id: "finger1",
+                    parameters: { pointerType: "touch" },
+                    actions: [
+                        {
+                            type: "pointerMove",
+                            duration: 0,
+                            x: width / 2,
+                            y: height * 0.9,
+                        },
+                        { type: "pointerDown", button: 0 },
+                        { type: "pause", duration: 100 },
+                        {
+                            type: "pointerMove",
+                            duration: 1000,
+                            x: width / 2,
+                            y: height * 0.1,
+                        },
+                        { type: "pointerUp", button: 0 },
+                    ],
+                },
+            ]);
+
+            await driver.pause(1000);
+
+
             const selectButton = await driver.$(
                 '-android uiautomator:new UiSelector().textContains("SELECT")',
             );
