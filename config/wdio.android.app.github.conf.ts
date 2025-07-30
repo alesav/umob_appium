c796512f5df687984b553cfcebf67f2cff327802
alekswhim <aleks.whim@gmail.com>
2025-07-30 21:30:00 +0300

    night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -107,10 +107,10 @@ export const config: WebdriverIO.Config = {
         // "../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts" //taking photo crashed app. Cant stop active booking for new32 user!
         //"../tests/specs/login/login.negative.spec.ts"
-        "../tests/specs/newUser/welcomeBookDonkey.spec.ts"
+        //"../tests/specs/newUser/welcomeBookDonkey.spec.ts"
 
            
-/*
+
 //night run
 
            "../tests/specs/account/*.spec.ts",
@@ -141,7 +141,7 @@ export const config: WebdriverIO.Config = {
           "../tests/specs/book/bookUmobMoped.spec.ts",
           "../tests/specs/book/reserveCheck.spec.ts",
           "../tests/specs/book/reserveFelyx.spec.ts"
-          */
+          
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
