7ad83bdaf7b9ec607dd32c3fbf8017c793935bb0
alekswhim <aleks.whim@gmail.com>
2025-08-05 22:40:39 +0300

    trying to merge

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -100,13 +100,13 @@ export const config: WebdriverIO.Config = {
     "../tests/specs/newUser/deleteCard.spec.ts"
 */
 
-    //tests for accept version of app
-    // "../tests/specs/login/login.negative.spec.ts",
-    // "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
-    // "../tests/specs/account/AddAdress.spec.ts", //new12
-    //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //newUser
-    // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
-    //"../tests/specs/newUser/deleteCard.spec.ts" //newUser
+        //tests for accept version of app
+        // "../tests/specs/login/login.negative.spec.ts",
+        // "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
+        // "../tests/specs/account/AddAdress.spec.ts", //new12 /test@gmail.com)
+        // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //newUser
+        // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
+        // "../tests/specs/newUser/deleteCard.spec.ts" //newUser (new43/test@gmail.com)
 
 
     ],
