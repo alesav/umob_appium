d5169174ec340117d390e3d41a4985d86f0c8162
alekswhim <aleks.whim@gmail.com>
2025-08-29 13:57:49 +0300

    update on felyx coordinates

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -17,7 +17,7 @@ export const config: WebdriverIO.Config = {
         // "../tests/specs/login/login.negative.spec.ts",
         //  "../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/book/bookTaxi.spec.ts",
-        //"../tests/specs/book/reserveFelyx.spec.ts",
+        "../tests/specs/book/reserveFelyx.spec.ts",
         // "../tests/specs/book/reserveCheck.spec.ts"
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
         //  "../tests/specs/book/bookUmobScooters.spec.js",
@@ -29,9 +29,9 @@ export const config: WebdriverIO.Config = {
         //  "..//tests/specs/newUser/nReserveCheckNoCard.spec.ts"
         //  "../tests/specs/account/combinedLoggedTest.spec.ts",
         //"../tests/specs/account/addPaymentMethod.spec.ts",
-        //  "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
-        //  "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
-        //  "../tests/specs/newUserVoucher/vBookFelyx.spec.ts"
+        "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
+        "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
+
         //  "../tests/specs/book/bookPublicTransport.spec.ts",
         // "../tests/specs/book/bookDonkeyMocked.spec.ts",
         /*
@@ -72,7 +72,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobScooters.spec.js", //more
         ////////"../tests/specs/book/reserveFelyx.spec.ts", //more
         //"../tests/specs/book/failedPaymentNoBooking.spec.ts", //more
-        //"../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
+        "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
@@ -85,7 +85,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/addAdress.spec.ts",
         //"../tests/specs/book/bookUmobScooters.spec.js",
         //"../tests/specs/account/addPaymentMethod.spec.ts", // ok
-        //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
+        "../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
         //"../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
@@ -95,7 +95,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ///////"../tests/specs/book/reserveCheck.spec.ts",
-        "../tests/specs/account/qrCode.spec.ts",
+        //"../tests/specs/account/qrCode.spec.ts",
         /////////"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
         //"../tests/specs/login/login.negative.spec.ts",
