ec56465c821003313075f921df3fdcf0d2537d1a
alekswhim <aleks.whim@gmail.com>
2025-09-01 21:30:06 +0300

    night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -53,7 +53,7 @@ export const config: WebdriverIO.Config = {
               "../tests/specs/newUser/nReserveFelyxNoId.spec.ts"
 
               */
-        "../tests/specs/book/reserveFelyx.spec.ts",
+        //"../tests/specs/book/reserveFelyx.spec.ts",
         //"../tests/specs/newUser/popupAddPaymentMethod.spec.ts",
         //    "../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //timeout problem
         //"../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
@@ -77,17 +77,17 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        "../tests/specs/book/bookTaxi.spec.ts",
+        //"../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
-        "../tests/specs/book/bookDonkeyMocked.spec.ts",
+        //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/account/addAdress.spec.ts",
         //"../tests/specs/book/bookUmobScooters.spec.js",
         //"../tests/specs/account/addPaymentMethod.spec.ts", // ok
         //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
-        "../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
-        "../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
+        //"../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
+        //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
         //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //Need Help. Test updated. Due to some reason there is still verification in progress problem
         //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //fail because payment card wasnt added
@@ -104,15 +104,15 @@ export const config: WebdriverIO.Config = {
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-        "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
-        "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
-        "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
+        //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
+        //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
+        //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
         //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
-        "../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
+        //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
-        "../tests/specs/newUser/deleteCard.spec.ts",
-        "../tests/specs/book/bookPublicTransport.spec.ts",
-        /*
+        //"../tests/specs/newUser/deleteCard.spec.ts",
+        //"../tests/specs/book/bookPublicTransport.spec.ts",
+
         //night run
 
         "../tests/specs/account/*.spec.ts",
@@ -141,7 +141,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
-        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
