b5ec3a4be77041ab913939a54b6e0a31c90f9e20
alekswhim <aleks.whim@gmail.com>
2025-09-08 09:33:57 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -76,7 +76,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        //"../tests/specs/book/bookTaxi.spec.ts",
+        "../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
@@ -84,7 +84,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/addAdress.spec.ts",
         //"../tests/specs/book/bookUmobScooters.spec.js",
         //"../tests/specs/account/addPaymentMethod.spec.ts", // ok
-        //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
+        "../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
         //"../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
@@ -93,11 +93,11 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts", // fail because payment card wasnt added
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-        ///////"../tests/specs/book/reserveCheck.spec.ts",
+        "../tests/specs/book/reserveCheck.spec.ts",
         //"../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
-        //"../tests/specs/login/login.negative.spec.ts",
+        "../tests/specs/login/login.negative.spec.ts",
         //"../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //new user tests should be in some order
@@ -109,9 +109,9 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
-        //"../tests/specs/newUser/deleteCard.spec.ts",
-        //"../tests/specs/book/bookPublicTransport.spec.ts",
-
+        "../tests/specs/newUser/deleteCard.spec.ts",
+        "../tests/specs/book/bookPublicTransport.spec.ts",
+        /*
         //night run
 
         "../tests/specs/account/*.spec.ts",
@@ -140,6 +140,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
+        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
