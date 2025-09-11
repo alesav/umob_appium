5f9cc1f3317530e5f40a9db2de618c6a0944ce41
alekswhim <aleks.whim@gmail.com>
2025-09-11 10:56:33 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -76,7 +76,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        "../tests/specs/book/bookTaxi.spec.ts",
+        //"../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
@@ -86,7 +86,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/addPaymentMethod.spec.ts", // ok
         //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //taking photo crashed app. Cant stop active booking for new37 user!
         //"../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
-        "../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
+        //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
         //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //Need Help. Test updated. Due to some reason there is still verification in progress problem
         //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //fail because payment card wasnt added
@@ -102,15 +102,15 @@ export const config: WebdriverIO.Config = {
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
-        "../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
+        ////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
-        "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
-        "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
+        //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
+        //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
         //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
-        "../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
+        //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
-        "../tests/specs/newUser/deleteCard.spec.ts",
-        "../tests/specs/book/bookPublicTransport.spec.ts",
+        //"../tests/specs/newUser/deleteCard.spec.ts",
+        //"../tests/specs/book/bookPublicTransport.spec.ts",
         /*
         //night run
 
