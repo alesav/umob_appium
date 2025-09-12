c8cb91d8af06086081b8e9e71091f35f879a417f
alekswhim <aleks.whim@gmail.com>
2025-09-12 21:29:28 +0300

    night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -111,8 +111,8 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/book/bookPublicTransport.spec.ts",
-        "../tests/specs/account/nearbyAssets.spec.ts",
-        /*
+        //"../tests/specs/account/nearbyAssets.spec.ts",
+
         //night run
 
         "../tests/specs/account/*.spec.ts",
@@ -141,7 +141,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
-        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
