63cf27a9923b71ad0abc2124946ec4308a45cfaa
alekswhim <aleks.whim@gmail.com>
2025-09-17 09:55:50 +0300

    day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -63,7 +63,7 @@ export const config: WebdriverIO.Config = {
         //  "../tests/specs/book/bookUmobMoped.spec.ts",
         // "../tests/specs/book/bookUmobBike.spec.ts"
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
-        //"../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
+        "../tests/specs/newUserVoucher/vCombinedLoggedTest.spec.ts",
         //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //ok
         //"../tests/specs/book/bookUmobMoped.spec.ts",
         //"../tests/specs/book/bookPublicTransport.spec.ts", //more
@@ -71,12 +71,12 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobScooters.spec.js", //more
         //"../tests/specs/book/reserveFelyx.spec.ts", //more
         //"../tests/specs/book/failedPaymentNoBooking.spec.ts", //more
-        //"../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
+        "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        //"../tests/specs/book/bookTaxi.spec.ts",
+        "../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
@@ -84,7 +84,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/addAdress.spec.ts",
         //"../tests/specs/book/bookUmobScooters.spec.js",
         //"../tests/specs/account/addPaymentMethod.spec.ts", // ok
-        //"../tests/specs/newUserVoucher/vBookCheck.spec.ts", //
+        "../tests/specs/newUserVoucher/vBookCheck.spec.ts", //
         //"../tests/specs/newUserVoucher/vBookDonkeyMocked.spec.ts", //ok/ ? fail, but there is no active booking for this user (new17)
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts", //test picked up moped from outside service area
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
@@ -102,17 +102,19 @@ export const config: WebdriverIO.Config = {
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
-        ////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-        //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
-        //"../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
+
+        "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
         //"../tests/specs/newUser/welcomeBookDonkey.spec.ts", //always required new welcome voucher
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
-        //"../tests/specs/newUser/deleteCard.spec.ts",
-        //"../tests/specs/book/bookPublicTransport.spec.ts",
+        "../tests/specs/newUser/deleteCard.spec.ts",
+        "../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
+        "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
+        "../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
         //night run
+        /*
 
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
@@ -140,6 +142,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
+        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
