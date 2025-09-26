d4caf0067e4c36bc4de518de19f214cd9b1ec06f
alekswhim <aleks.whim@gmail.com>
2025-09-26 11:02:43 +0300

    26/09/2025 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -110,7 +110,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-        //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
+        "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
         "../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
         ////////////////////////////////////////////////////////////////////////////////////////
