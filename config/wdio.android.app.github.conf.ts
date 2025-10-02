46a96617cfe25313faf9950dacac6216f33cf648
alekswhim <aleks.whim@gmail.com>
2025-10-02 09:54:46 +0300

    02/10/2025 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -76,7 +76,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts",
         // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
-        "../tests/specs/book/bookTaxi.spec.ts",
+        //"../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         //"../tests/specs/account/combinedLoggedTest.spec.ts",
@@ -95,10 +95,10 @@ export const config: WebdriverIO.Config = {
         //////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ////////"../tests/specs/book/reserveCheck.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
-        //"../tests/specs/account/qrCode.spec.ts",
+        "../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
-        "../tests/specs/login/login.negative.spec.ts",
+        //"../tests/specs/login/login.negative.spec.ts",
         //"../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         /////////////////////////////////////////////////////////////////////////////////////////////
@@ -112,8 +112,8 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
-        "../tests/specs/book/bookPublicTransport.spec.ts",
-        "../tests/specs/account/nearbyAssets.spec.ts",
+        //"../tests/specs/book/bookPublicTransport.spec.ts",
+        //"../tests/specs/account/nearbyAssets.spec.ts",
         ////////////////////////////////////////////////////////////////////////////////////////
         /*
         //night run
