5a48a673e90570e3e5967e5b1dcbce59fea641d5
alekswhim <aleks.whim@gmail.com>
2025-10-14 21:30:36 +0300

    14/10/2025 night run with updates

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -95,7 +95,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ////////"../tests/specs/book/reserveCheck.spec.ts",
-        "../tests/specs/account/nearbyAssets.spec.ts",
+        //"../tests/specs/account/nearbyAssets.spec.ts",
         //"../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
@@ -116,7 +116,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
         ////////////////////////////////////////////////////////////////////////////////////////
-        /*
+
         //night run
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
@@ -145,7 +145,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
-        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
