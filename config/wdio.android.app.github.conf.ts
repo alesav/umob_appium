3fa526751f20994450eb94b6ae630389764604f0
alekswhim <aleks.whim@gmail.com>
2025-10-24 10:48:10 +0300

    24/10/2025 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -102,7 +102,7 @@ export const config: WebdriverIO.Config = {
         // "../tests/specs/login/login.negative.spec.ts",
         // "../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
-        "../tests/specs/book/bookPublicTransport.spec.ts",
+        ///"../tests/specs/book/bookPublicTransport.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
