fb7b6c69a5ff245122d5fd28be1d9b02747722b4
alekswhim <aleks.whim@gmail.com>
2025-10-27 19:17:47 +0300

    27/10/2025 update pageobject and booking felyx test

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -78,7 +78,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts",
         //"../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/newUser/nReserveFelyxNoId.spec.ts",
-        "../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
+        //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         //"../tests/specs/book/bookDonkeyMocked.spec.ts",
         // "../tests/specs/account/combinedLoggedTest.spec.ts",
         //"../tests/specs/newUser/deleteCard.spec.ts",
@@ -96,7 +96,7 @@ export const config: WebdriverIO.Config = {
         //////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ////////"../tests/specs/book/reserveCheck.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
-        "../tests/specs/account/qrCode.spec.ts",
+        //"../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
         //"../tests/specs/login/login.negative.spec.ts",
@@ -114,7 +114,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
-        "../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
+        //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         ////////////////////////////////////////////////////////////////////////////////////////
         /*
         //night run
