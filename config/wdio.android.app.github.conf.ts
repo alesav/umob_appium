e100ccf4119fb454c85d9573e29a92fb3f9120da
alesav <4bigfoot@gmail.com>
2025-10-27 15:17:32 +0300

    Posthog test

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -96,14 +96,14 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ////////"../tests/specs/book/reserveCheck.spec.ts",
-        "../tests/specs/account/nearbyAssets.spec.ts",
-        "../tests/specs/account/qrCode.spec.ts",
+        //"../tests/specs/account/nearbyAssets.spec.ts",
+        //"../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
         // "../tests/specs/login/login.negative.spec.ts",
         // "../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
-        "../tests/specs/book/bookPublicTransport.spec.ts",
+        //"../tests/specs/book/bookPublicTransport.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
