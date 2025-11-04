ac15abe60987ee1288ded98cb5db9a77c862486a
alekswhim <aleks.whim@gmail.com>
2025-11-04 10:14:53 +0300

    04/11/2025 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -69,7 +69,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookPublicTransport.spec.ts", //more
         //"../tests/specs/newUser/welcomeBookDonkey.spec.ts",
         //"../tests/specs/book/bookUmobScooters.spec.js", //more
-        //"../tests/specs/book/reserveFelyx.spec.ts", //more
+        "../tests/specs/book/reserveFelyx.spec.ts", //more
         //"../tests/specs/book/failedPaymentNoBooking.spec.ts", //more
         // "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
         // "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //ok
@@ -96,13 +96,13 @@ export const config: WebdriverIO.Config = {
         //////////"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         ////////"../tests/specs/book/reserveCheck.spec.ts",
         //"../tests/specs/account/nearbyAssets.spec.ts",
-        //"../tests/specs/account/qrCode.spec.ts",
+        "../tests/specs/account/qrCode.spec.ts",
         //"../tests/specs/book/bookUmobBike.spec.ts", //location problems were before
         //"../tests/specs/book/bookUmobMoped.spec.ts", //taking photo crashed app. Cant stop active booking for new32 user!
         //"../tests/specs/login/login.negative.spec.ts",
-        //"../tests/specs/login/login.positive.spec.ts",
+        "../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
-        //"../tests/specs/book/bookPublicTransport.spec.ts",
+        "../tests/specs/book/bookPublicTransport.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
@@ -115,9 +115,9 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
         //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
-        //"../tests/specs/book/bookDott.spec.ts",
+        "../tests/specs/book/bookDott.spec.ts",
         ////////////////////////////////////////////////////////////////////////////////////////
-
+        /*
         //night run
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
@@ -147,6 +147,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
+        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
