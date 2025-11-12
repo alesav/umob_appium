6d091be5cad81bf7064db65498d7295c4eb1484c
alekswhim <aleks.whim@gmail.com>
2025-11-12 19:59:37 +0300

    12/11/2025 night run

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -102,12 +102,12 @@ export const config: WebdriverIO.Config = {
     
 */
         //TESTS FOR ACCEPT APP (npm run android.app.test.accept)
-        "../tests/specs/login/login.negative.spec.ts",
-        "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
-        "../tests/specs/account/AddAdress.spec.ts", //new12 /test@gmail.com)
-        "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //newUser
-        "../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
-        "../tests/specs/newUser/deleteCard.spec.ts", //newUser (new48/test@gmail.com)
+        // "../tests/specs/login/login.negative.spec.ts",
+        // "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
+        // "../tests/specs/account/AddAdress.spec.ts", //new12 /test@gmail.com)
+        // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //newUser
+        // "../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
+        // "../tests/specs/newUser/deleteCard.spec.ts", //newUser (new48/test@gmail.com)
     ],
     capabilities: [
         {
