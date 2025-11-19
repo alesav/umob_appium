a60fc7b6e4caf7ceac6d4f3fee201ee7c14b5f04
alekswhim <aleks.whim@gmail.com>
2025-11-19 12:37:26 +0300

    19/11/2025 day run

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -102,7 +102,7 @@ export const config: WebdriverIO.Config = {
     
 */
         //TESTS FOR ACCEPT APP (npm run android.app.test.accept)
-        // "../tests/specs/login/login.negative.spec.ts",
+        "../tests/specs/login/login.negative.spec.ts",
         // "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
         // "../tests/specs/account/AddAdress.spec.ts", //new12 /test@gmail.com)
         // "../tests/specs/newUser/COPYpopupAddPaymentMethod.spec.ts", //newUser
