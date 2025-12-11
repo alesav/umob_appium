8d09a9f9d949bd87cb2aed02718c16d279953790
alekswhim <aleks.whim@gmail.com>
2025-12-11 10:38:21 +0300

    11/12/2025 day run

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -94,7 +94,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/nearbyAssets.spec.ts", //new12
         //"../tests/specs/login/login.negative.spec.ts",
         //"../tests/specs/login/login.positive.spec.ts",
-        //"../tests/specs/book/bookDott.spec.ts",
+        "../tests/specs/book/bookDott.spec.ts",
         /*
 //new user tests should be in some order 
     "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",      //was new13  //now newUser in credentials (new48)
