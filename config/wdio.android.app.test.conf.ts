1b71a50817ba402c16e31f24e6f9e31347e895d4
alekswhim <aleks.whim@gmail.com>
2025-12-15 10:19:49 +0300

    15/12/2025 day run

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -65,7 +65,7 @@ export const config: WebdriverIO.Config = {
         //morning run
         //"../tests/specs/book/reserveFelyx.spec.ts",
         // "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js",
-        // "../tests/specs/book/failedPaymentNoBooking.spec.ts",
+        "../tests/specs/book/failedPaymentNoBooking.spec.ts",
         // "../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         // "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
         //"c:/dev/umob_appium/tests/specs/book/bookPublicTransport.spec.ts", //new35 //ok
@@ -94,7 +94,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/account/nearbyAssets.spec.ts", //new12
         //"../tests/specs/login/login.negative.spec.ts",
         //"../tests/specs/login/login.positive.spec.ts",
-        "../tests/specs/book/bookDott.spec.ts",
+        //"../tests/specs/book/bookDott.spec.ts",
         /*
 //new user tests should be in some order 
     "../tests/specs/newUser/nCombinedLoggedTest.spec.ts",      //was new13  //now newUser in credentials (new48)
