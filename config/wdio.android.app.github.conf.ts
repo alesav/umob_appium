f080daecd6f43db67da28791e2ed74de19ec295f
alekswhim <aleks.whim@gmail.com>
2025-12-19 11:51:14 +0300

    19/12/2025 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -99,14 +99,14 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/login/login.negative.spec.ts",
         //"../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
-        "../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
+        //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         //"../tests/specs/book/failedPaymentNoBooking.spec.ts",
-        //"../tests/specs/book/bookTaxi.spec.ts",
+        "../tests/specs/book/bookTaxi.spec.ts",
         //"../tests/specs/book/reserveFelyx.spec.ts",
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
         // "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
         //"../tests/specs/book/bookPublicTransport.spec.ts",
-        "../tests/specs/book/bookDott.spec.ts",
+        //"../tests/specs/book/bookDott.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
