67737c464b82e260211aa0c5d7610cd1770c4ce4
alekswhim <aleks.whim@gmail.com>
2026-01-03 00:28:10 +0300

    02/01/2065 night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -100,14 +100,14 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
-        "../tests/specs/book/failedPaymentNoBooking.spec.ts",
+        //"../tests/specs/book/failedPaymentNoBooking.spec.ts",
         //"../tests/specs/book/bookTaxi.spec.ts",
-        "../tests/specs/book/reserveFelyx.spec.ts",
+        //"../tests/specs/book/reserveFelyx.spec.ts",
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
         // "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
         //"../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/book/bookDott.spec.ts",
-        "../tests/specs/account/registration.spec.ts",
+        //"../tests/specs/account/registration.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
         //new user tests should be in some order
         //"../tests/specs/newUser/*.spec.ts",
@@ -123,7 +123,6 @@ export const config: WebdriverIO.Config = {
         ////////////////////////////////////////////////////////////////////////////////////////
 
         //night run
-        /*
 
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
@@ -153,7 +152,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
-        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
