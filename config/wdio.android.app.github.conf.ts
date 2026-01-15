685cade2a29a53d8dc399803f94fb835157dc2e3
alekswhim <aleks.whim@gmail.com>
2026-01-15 10:33:51 +0300

    15/01/2026 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -101,7 +101,7 @@ export const config: WebdriverIO.Config = {
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         "../tests/specs/book/failedPaymentNoBooking.spec.ts",
-        "../tests/specs/book/newBookTaxi.spec.ts",
+        //"../tests/specs/book/newBookTaxi.spec.ts",
         //"../tests/specs/book/reserveFelyx.spec.ts",
         //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
         // "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
