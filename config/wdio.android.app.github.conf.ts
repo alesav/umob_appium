be0bf7326fd07a1e2e52c4641d9426503a287e7f
alekswhim <aleks.whim@gmail.com>
2026-01-16 10:59:58 +0300

    16/01/2026 day run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -100,12 +100,12 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/login/login.positive.spec.ts",
         ///"../tests/specs/newUser/welcomeBookDonkey.spec.ts" //only once for the welcome voucher
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
-        //"../tests/specs/book/failedPaymentNoBooking.spec.ts",
-        //"../tests/specs/book/newBookTaxi.spec.ts",
+        "../tests/specs/book/failedPaymentNoBooking.spec.ts",
+        "../tests/specs/book/newBookTaxi.spec.ts",
         //"../tests/specs/book/reserveFelyx.spec.ts",
-        //"../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
+        "../tests/specs/newUserVoucher/vBookFelyx.spec.ts",
         // "../tests/specs/newUserVoucher/vBookCheck.spec.ts",
-        //"../tests/specs/book/bookPublicTransport.spec.ts",
+        "../tests/specs/book/bookPublicTransport.spec.ts",
         //"../tests/specs/book/bookDott.spec.ts",
         //"../tests/specs/account/registration.spec.ts",
         /////////////////////////////////////////////////////////////////////////////////////////////
@@ -118,11 +118,11 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/addVoucher.spec.ts",// voucher could be added to different users but only once for each user
         //"../tests/specs/newUser/deleteCard.spec.ts",
         //"../tests/specs/newUser/nReserveCheckNoCard.spec.ts",
-        //"../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
+        "../tests/specs/newUser/nReserveDonkeyNoCard.spec.js", //js file
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         ////////////////////////////////////////////////////////////////////////////////////////
         //night run
-
+        /*
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
         "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
@@ -151,6 +151,7 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
+        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
