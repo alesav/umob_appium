6ce205c1f9c1d46dfbeacde5cff2ba16902e83b6
alekswhim <aleks.whim@gmail.com>
2026-01-28 19:47:17 +0300

    28/01/2026 night run

--- config/wdio.android.app.github.conf.ts
+++ config/wdio.android.app.github.conf.ts
@@ -123,7 +123,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/newUser/nReserveFelyxNoCard.spec.js",
         ////////////////////////////////////////////////////////////////////////////////////////
         //night run
-        /*
 
         "../tests/specs/account/*.spec.ts",
         "../tests/specs/login/*.spec.ts",
@@ -145,7 +144,7 @@ export const config: WebdriverIO.Config = {
 
         //book folder
         "../tests/specs/book/bookDott.spec.ts",
-        "../tests/specs/book/bookPublicTransport.spec.ts",
+        "../tests/specs/book/newBookPublicTransport.spec.ts",
         "../tests/specs/book/newBookTaxi.spec.ts",
         "../tests/specs/book/bookDonkeyMocked.spec.ts",
         "../tests/specs/book/failedPaymentNoBooking.spec.ts",
@@ -154,7 +153,6 @@ export const config: WebdriverIO.Config = {
         //"../tests/specs/book/bookUmobMoped.spec.ts", //temporarily excluded negative scenario because of blocking user after runs
         "../tests/specs/book/reserveCheck.spec.ts",
         "../tests/specs/book/reserveFelyx.spec.ts",
-        */
     ],
 
     // specs: ["../tests/specs/account/*.spec.ts",
