e41d953a7b135c0008dc324972c7e62c882487da
alekswhim <aleks.whim@gmail.com>
2026-02-02 11:35:58 +0300

    02/02/2026 day run

--- tests/specs/account/locationPermissionOff.spec.ts
+++ tests/specs/account/locationPermissionOff.spec.ts
@@ -144,6 +144,27 @@ describe("Test for checking disability of aplication features when location perm
             ]);
             await driver.pause(2000);
 
+            await driver.performActions([
+                {
+                    type: "pointer",
+                    id: "finger2",
+                    parameters: { pointerType: "touch" },
+                    actions: [
+                        {
+                            type: "pointerMove",
+                            duration: 0,
+                            x: 160,
+                            y: height / 2,
+                        },
+                        { type: "pointerDown", button: 0 },
+                        { type: "pause", duration: 100 },
+                        { type: "pointerMove", duration: 1000, x: 160, y: 10 },
+                        { type: "pointerUp", button: 0 },
+                    ],
+                },
+            ]);
+            await driver.pause(2000);
+
             //verify enable location button for nearby assets
             const buttonForNearbyAssets = await driver.$(
                 '-android uiautomator:new UiSelector().text("Enable location to find")',
