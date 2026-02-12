3fc81ffa9e6dfc9a90a3ea23a5d3e79d3067edaf
alekswhim <aleks.whim@gmail.com>
2026-02-12 23:52:27 +0300

    12/02/2026 night run

--- config/wdio.android.app.test.conf.ts
+++ config/wdio.android.app.test.conf.ts
@@ -112,13 +112,13 @@ export const config: WebdriverIO.Config = {
     "../tests/specs/newUser/deleteCard.spec.ts"
     
 */
-        //TESTS FOR ACCEPT APP (npm run android.app.test.accept)
-        //"../tests/specs/login/login.negative.spec.ts",
-        //"../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
-        //"../tests/specs/account/AddAdress.spec.ts", //new12 /test2@gmail.com)
-        //"../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
-        //"../tests/specs/newUser/addPaymentMethod.spec.ts", //newUser
-        //"../tests/specs/newUser/deleteCard.spec.ts", //newUser (new48/test2@gmail.com)
+        //TESTS FOR ACCEPT APP (npm run android.app.accept)
+        "../tests/specs/login/login.negative.spec.ts",
+        "../tests/specs/notLoggedTests/combinedNotLogged.spec.ts",
+        "../tests/specs/account/AddAdress.spec.ts", //new12 /test2@gmail.com)
+        "../tests/specs/newUser/nCombinedLoggedTest.spec.ts", //newUser
+        "../tests/specs/newUser/addPaymentMethod.spec.ts", //newUser
+        "../tests/specs/newUser/deleteCard.spec.ts", //newUser (new48/test2@gmail.com)
     ],
     capabilities: [
         {
